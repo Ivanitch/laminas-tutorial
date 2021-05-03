add6a135b9df4a04920ccaa4519c3d0a23b8e673
Sergey Ivanov <ivanitch.mail@gmail.com>
2021-05-03 20:59:54 +0900

    Routing and controllers

---
+++ module/Album/src/Controller/AlbumController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Album\Controller;
+
+use Laminas\Mvc\Controller\AbstractActionController;
+
+class AlbumController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+
+    }
+
+    public function deleteAction()
+    {
+    }
+}
