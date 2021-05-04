afa5dea866a380a46099f8556f84e8d193d30441
Sergey Ivanov <ivanitch.mail@gmail.com>
2021-05-04 18:19:51 +0900

    Unit Testing

---
+++ module/Album/test/Controller/AlbumControllerTest.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlbumTest\Controller;
+
+use Album\Controller\AlbumController;
+use Album\Model\Album;
+use Album\Model\AlbumTable;
+use Laminas\ServiceManager\ServiceManager;
+use Laminas\Stdlib\ArrayUtils;
+use Laminas\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Prophecy\Prophecy\ObjectProphecy;
+
+class AlbumControllerTest extends AbstractHttpControllerTestCase
+{
+    use ProphecyTrait;
+
+    protected $traceError = true;
+
+    protected $albumTable;
+
+    protected function setUp() : void
+    {
+        $configOverrides = [];
+
+        $this->setApplicationConfig(ArrayUtils::merge(
+            include __DIR__ . '/../../../../config/application.config.php',
+            $configOverrides
+        ));
+        parent::setUp();
+
+        $this->configureServiceManager($this->getApplicationServiceLocator());
+    }
+
+    public function testIndexActionCanBeAccessed()
+    {
+        $this->albumTable->fetchAll()->willReturn([]);
+
+        $this->dispatch('/album');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('Album');
+        $this->assertControllerName(AlbumController::class);
+        $this->assertControllerClass('AlbumController');
+        $this->assertMatchedRouteName('album');
+    }
+
+
+    public function testAddActionRedirectsAfterValidPost()
+    {
+        $this->albumTable
+            ->saveAlbum(Argument::type(Album::class))
+            ->shouldBeCalled();
+
+        $postData = [
+            'title'  => 'Led Zeppelin III',
+            'artist' => 'Led Zeppelin',
+            'id'     => '',
+        ];
+        $this->dispatch('/album/add', 'POST', $postData);
+        $this->assertResponseStatusCode(302);
+        $this->assertRedirectTo('/album');
+    }
+
+
+
+    protected function configureServiceManager(ServiceManager $services)
+    {
+        $services->setAllowOverride(true);
+
+        $services->setService('config', $this->updateConfig($services->get('config')));
+        $services->setService(AlbumTable::class, $this->mockAlbumTable()->reveal());
+
+        $services->setAllowOverride(false);
+    }
+
+    protected function updateConfig($config)
+    {
+        $config['db'] = [];
+        return $config;
+    }
+
+    protected function mockAlbumTable(): ObjectProphecy
+    {
+        $this->albumTable = $this->prophesize(AlbumTable::class);
+        return $this->albumTable;
+    }
+}
