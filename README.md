47723d5f52637a82482675c2aec535247df9ae6e
Sergey Ivanov <ivanitch.mail@gmail.com>
2021-05-18 11:49:03 +0900

    Configuration

--- README.md
+++ README.md
@@ -5,7 +5,27 @@ A Tutorial for Building an Application Using Laminas
 * [Laminas Tutorial](https://docs.laminas.dev/tutorials/)
 * [laminas-mvc-skeleton](https://github.com/laminas/laminas-mvc-skeleton)
 
+### Development mode
+```shell
+# Let's enable development mode
+composer development-enable
+
+# status development mode
+composer development-status
+
+# disable development mode
+composer development-disable
+```
+### Development config
+```shell
+# Copying the local config file
+composer local
+
+# Removing the local config file
+composer production
+```
 ### TODO
 Revision required
 * [Internationalization (laminas-i18n)](https://docs.laminas.dev/laminas-i18n/)
+* [Unit Testing](https://docs.laminas.dev/tutorials/unit-testing/#testing-actions-with-post)
 
--- public/index.php
+++ public/index.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 use Laminas\Mvc\Application;
 use Laminas\Stdlib\ArrayUtils;
 
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
+if ($_SERVER['APPLICATION_ENV'] === 'development') {
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+}
+
 chdir(dirname(__DIR__));
 
-// Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
     $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
     if (is_string($path) && __FILE__ !== $path && is_file($path)) {
@@ -20,8 +21,7 @@ if (php_sapi_name() === 'cli-server') {
     unset($path);
 }
 
-// Composer autoloading
-include __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 if (! class_exists(Application::class)) {
     throw new RuntimeException(
@@ -32,11 +32,9 @@ if (! class_exists(Application::class)) {
     );
 }
 
-// Retrieve configuration
 $appConfig = require __DIR__ . '/../config/application.config.php';
 if (file_exists(__DIR__ . '/../config/development.config.php')) {
     $appConfig = ArrayUtils::merge($appConfig, require __DIR__ . '/../config/development.config.php');
 }
 
-// Run the application!
 Application::init($appConfig)->run();
