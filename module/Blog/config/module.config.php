51aba2809b42f3e8e6dd0a17f07fa51c67e656fb
Sergey Ivanov <ivanitch.mail@gmail.com>
2021-05-24 13:36:36 +0900

    Making Use of Forms and Fieldsets

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -10,15 +10,19 @@ return [
     'service_manager' => [
         'aliases' => [
             Model\PostRepositoryInterface::class => Model\LaminasDbSqlRepository::class,
+            Model\PostCommandInterface::class => Model\LaminasDbSqlCommand::class,
         ],
         'factories' => [
             Model\PostRepository::class => InvokableFactory::class,
             Model\LaminasDbSqlRepository::class => Factory\LaminasDbSqlRepositoryFactory::class,
+            Model\PostCommand::class => InvokableFactory::class,
+            Model\LaminasDbSqlCommand::class => Factory\LaminasDbSqlCommandFactory::class,
         ],
     ],
     'controllers' => [
         'factories' => [
             Controller\ListController::class => Factory\ListControllerFactory::class,
+            Controller\WriteController::class => Factory\WriteControllerFactory::class,
         ],
     ],
     'router'          => [
@@ -43,6 +47,16 @@ return [
                             ],
                             'constraints' => [
                                 'id' => '[1-9]\d*',
+                            ],
+                        ],
+                    ],
+                    'add' => [
+                        'type' => Literal::class,
+                        'options' => [
+                            'route'    => '/add',
+                            'defaults' => [
+                                'controller' => Controller\WriteController::class,
+                                'action'     => 'add',
                             ],
                         ],
                     ],
---
+++ module/Blog/src/Model/LaminasDbSqlCommand.php
@@ -0,0 +1,63 @@
+<?php
+namespace Blog\Model;
+
+use RuntimeException;
+use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Db\Adapter\Driver\ResultInterface;
+use Laminas\Db\Sql\Delete;
+use Laminas\Db\Sql\Insert;
+use Laminas\Db\Sql\Sql;
+use Laminas\Db\Sql\Update;
+
+class LaminasDbSqlCommand implements PostCommandInterface
+{
+    /**
+     * @var AdapterInterface
+     */
+    private $db;
+
+    /**
+     * @param AdapterInterface $db
+     */
+    public function __construct(AdapterInterface $db)
+    {
+        $this->db = $db;
+    }
+
+    public function insertPost(Post $post): Post
+    {
+        $insert = new Insert('post');
+        $insert->values([
+            'title' => $post->getTitle(),
+            'text' => $post->getText(),
+        ]);
+
+        $sql = new Sql($this->db);
+        $statement = $sql->prepareStatementForSqlObject($insert);
+        $result = $statement->execute();
+
+        if (! $result instanceof ResultInterface) {
+            throw new RuntimeException(
+                'Database error occurred during blog post insert operation'
+            );
+        }
+
+        $id = $result->getGeneratedValue();
+
+        return new Post(
+            $post->getTitle(),
+            $post->getText(),
+            $id
+        );
+    }
+
+    public function updatePost(Post $post): Post
+    {
+        // TODO: Implement updatePost() method.
+    }
+
+    public function deletePost(Post $post): bool
+    {
+        // TODO: Implement deletePost() method.
+    }
+}
